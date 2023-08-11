a1c2949589bf6ac8b2bb58e061f1ac2eacd51123
alen <fation.qmam.god@gmail.com>
2023-08-11 18:43:34 +0200

    feat: add getallfields function

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import InputList from './components/InputList.vue';
+import { store } from './store';
+import { ActionType } from './store/actions';
+onMounted(() => store.dispatch(ActionType.GetAllFields))
 </script>
 
 <template>
