73bd6191f0196f99466edf808e7da2afe734b82c
alen <fation.qmam.god@gmail.com>
2023-08-11 21:37:02 +0200

    feat: add remove inputfield function

--- src/components/InputItem.vue
+++ src/components/InputItem.vue
@@ -4,9 +4,22 @@
             <input type="text" id="hs-trailing-button-add-on" name="hs-trailing-button-add-on"
                 class="py-3 px-4 block w-full border-2 border-slate-900 rounded-l-md text-sm focus:z-10 focus:border-sky-500 focus:ring-blue-500">
             <button type="button"
-                class="py-3 px-4 inline-flex flex-shrink-0 justify-center items-center gap-2 rounded-r-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all text-sm">
+                class="py-3 px-4 inline-flex flex-shrink-0 justify-center items-center gap-2 rounded-r-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all text-sm"
+                @click="remove(id)">
                 Remove
             </button>
         </div>
     </div>
 </template>
+<script setup lang="ts">
+import { useStore } from '../store';
+import { ActionType } from '../store/actions';
+
+const { id } = defineProps<{
+    id: Number
+}>()
+const store = useStore()
+const remove = (id: Number) => {
+    store.dispatch(ActionType.RemoveField, id);
+}
+</script>../store/actionsd.../store/store
