5b81d7b277fb40b866b8857288b3e3cd914a51fc
alen <fation.qmam.god@gmail.com>
2023-08-12 22:04:25 +0200

    feat: validation for each fields

--- src/components/InputList.vue
+++ src/components/InputList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container w-[500px]">
         <div class="mt-[200px]">
-            <form>
+            <form action="" @submit.prevent="submit">
                 <div class="flex justify-end mb-7">
                     <button type="button"
                         class="py-3 px-4 inline-flex flex-shrink-0 justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all text-sm"
@@ -9,11 +9,22 @@
                         Add
                     </button>
                 </div>
-                <div v-for="(_, index) in fields" :key="index">
-                    <InputItem :id="index" :remove="remove" />
+                <div v-for="(_, index) in fields.collection" :key="index">
+                    <div class="flex justify-center mb-5">
+                        <div class="flex rounded-md shadow-sm w-full">
+                            <input v-model="v.collection.$model[index].fieldValue" type="text"
+                                :class="{ 'border-2 border-rose-500': v.collection.$each.$response.$data[index].fieldValue.$error && v.$dirty, 'border-2 border-green-500': !v.collection.$each.$response.$data[index].fieldValue.$error }"
+                                class="py-3 px-4 block w-full border-2 border-slate-900 rounded-l-md text-sm focus:z-10 focus:border-sky-500 focus:ring-blue-500">
+                            <button type="button"
+                                class="py-3 px-4 inline-flex flex-shrink-0 justify-center items-center gap-2 rounded-r-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all text-sm"
+                                @click="remove(index)">
+                                Remove
+                            </button>
+                        </div>
+                    </div>
                 </div>
                 <div class="flex justify-between mt-7">
-                    <button type="button"
+                    <button type="submit"
                         class="py-3 px-4 inline-flex w-full flex-shrink-0 justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all text-sm">
                         Submit
                     </button>
@@ -24,19 +35,42 @@
 </template>
 <script setup lang="ts">
 import { reactive } from 'vue';
-import InputItem from './InputItem.vue';
-const fields = reactive([
-    {
-        value: ""
-    },
-    {
-        value: ""
+import { required, helpers } from '@vuelidate/validators'
+import { useVuelidate } from '@vuelidate/core'
+
+
+const fields = reactive({
+    collection: [
+        { fieldValue: "" }, { fieldValue: "" }
+    ]
+})
+
+const rules = {
+    collection: {
+        $each: helpers.forEach({
+            fieldValue: {
+                required
+            }
+        })
     }
-])
-const add = () => {
-    fields.push({ value: "" });
 }
+
+const v = useVuelidate(rules, fields);
+
+const add = () => {
+    fields.collection.push({ fieldValue: "" });
+}
+
 const remove = (index: number) => {
-    fields.splice(index, 1);
+    fields.collection.splice(index, 1);
+}
+
+const submit = () => {
+    v.value.$touch();
+    if (!v.value.$invalid) {
+        alert("Success");
+    } else {
+        alert("Form not valid");
+    }
 }
 </script>
