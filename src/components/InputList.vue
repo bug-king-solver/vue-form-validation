2151b6af1da30119b6c87699da491e996e2226a7
alen <fation.qmam.god@gmail.com>
2023-08-13 16:03:11 +0200

    style(InputList): change style of input box

--- src/components/InputList.vue
+++ src/components/InputList.vue
@@ -13,8 +13,9 @@
                     <div class="flex justify-center mb-5">
                         <div class="flex rounded-md shadow-sm w-full">
                             <input v-model="v.collection.$model[index].fieldValue" type="text"
-                                :class="{ 'border-2 border-rose-500': v.collection.$each.$response.$data[index].fieldValue.$error && v.$dirty, 'border-2 border-green-500': !v.collection.$each.$response.$data[index].fieldValue.$error }"
-                                class="py-3 px-4 block w-full border-2 border-slate-900 rounded-l-md text-sm focus:z-10 focus:border-sky-500 focus:ring-blue-500">
+                            :class="{ 'border-2 border-rose-500': v.collection.$each.$response.$data[index].fieldValue.$error && v.$dirty, 'border-2 border-green-500': !v.collection.$each.$response.$data[index].fieldValue.$error }"
+                            class="block w-full border-2 text-gray-900 focus:border-sky-500"
+                            >
                             <button type="button"
                                 class="py-3 px-4 inline-flex flex-shrink-0 justify-center items-center gap-2 rounded-r-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all text-sm"
                                 @click="remove(index)">
